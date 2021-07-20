20afbc9494e4b252b25f661d521353b52c1a3490
Tyler Hyde <tyler.hyde@sap.com>
2021-07-20 16:39:29 -0400

    end of day 20 july, need to work with ben on the modal and finalize the colors tomorrow

--- Organization-Management-ERP/src/app/tab5/tab5.page.ts
+++ Organization-Management-ERP/src/app/tab5/tab5.page.ts
@@ -3,6 +3,7 @@ import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
+import { UserInfoService } from '../user-info.service';
 
 @Component({
   selector: 'app-tab5',
@@ -11,13 +12,32 @@ import { Router } from '@angular/router';
 })
 export class Tab5Page implements OnInit {
 
-  constructor(private afAuth:AngularFireAuth,private afData:AngularFireDatabase,public router:Router) { }
+  usrData: any
+  constructor(private afAuth:AngularFireAuth,private afData:AngularFireDatabase,public router:Router, public uInfo: UserInfoService) { }
 
-  ngOnInit() {
+  
+
+  
+  loadUserInfo(){
+    let global = this;
+    this.usrData =  this.uInfo.$usrData
+    if(this.uInfo.$usrData == undefined){
+      setTimeout(() => {
+        this.loadUserInfo()
+      }, 1000);
+    }else{
+      this.uInfo.$usrData.subscribe({
+        next(data){
+          global.usrData = data;
+          console.log(data)
+        }
+      })
+    }
   }
 
-
-
+  ngOnInit() {
+    this.loadUserInfo()
+  }
 
   
   logOut(){
