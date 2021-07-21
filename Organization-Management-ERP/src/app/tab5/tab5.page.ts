4a27d6614bf8a5a47dc30375475913bdd8157ea5
Tyler Hyde <tyler.hyde@sap.com>
2021-07-21 16:37:28 -0400

    end of day commit, got the profile stuff updating the back end, will update the style tomorrow

--- Organization-Management-ERP/src/app/tab5/tab5.page.ts
+++ Organization-Management-ERP/src/app/tab5/tab5.page.ts
@@ -13,13 +13,17 @@ import { UserInfoService } from '../user-info.service';
 export class Tab5Page implements OnInit {
   toggle: boolean = false
   usrData: any
+  firstname: any
+  lastname: any
+  emailaddress: any
+  phonenumber: any
   constructor(private afAuth:AngularFireAuth,private afData:AngularFireDatabase,public router:Router, public uInfo: UserInfoService) { }
 
   status=false;
   
   onChange(){
     if(this.status){
-      this.buttonOn();
+      this.buttonOn()
       // this.status = false
     }
     else{
@@ -35,11 +39,17 @@ export class Tab5Page implements OnInit {
     console.log("the button is off")
       
   }
-  // buttonOn(){
-  //    ("apple")
-  //   console.log("the button is on")
-  // }
-  
+
+  confirmEdit(){
+    // this.usrData.firstname = this.firstname
+    // console.log(this.usrData.usrId)
+    this.afData.database.ref("users").child(this.usrData.id).update({"firstname":this.firstname, "lastname": this.lastname})
+    console.log(this.firstname)
+    console.log(this.lastname)
+    console.log(this.emailaddress)
+    this.status=false
+  }
+
   loadUserInfo(){
     let global = this;
     this.usrData =  this.uInfo.$usrData
